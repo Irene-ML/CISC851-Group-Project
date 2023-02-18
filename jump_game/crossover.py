7cf2aab3672408381f5bce82d11a94470eb2b46a
Chen Xu <chenxu@chens-mbp.lan>
2023-02-18 09:46:55 -0500

    Designed crossover algorithm

--- jump_game/crossover.py
+++ jump_game/crossover.py
@@ -1,3 +1,20 @@
 """_description_
 """
+import nn
 
+def crossover(agent1, agent2):
+        """
+        Apply crossover on the chronosome of two agents
+        Args:
+            agent1, agent2 (nn): nn objects
+        """
+    offspring1 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring2 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    
+    offspring1.w_ih = agent1.w_ih
+    offspring1.w_ho = agent2.w_ho
+    
+    offspring2.w_ih = agent2.w_ih
+    offspring2.w_ho = agent1.w_ho
+
+    return offspring1, offspring2
--- jump_game/mutation.py
+++ jump_game/mutation.py
@@ -1,3 +1,20 @@
 """_description_
 """
+import nn
 
+def uncorr_onestep(agent, sigma):
+    """
+    Apply uncorrelated update on genes of agent
+    Args:
+        agent (nn): An agent
+        sigma (float): step size
+    """
+    
+    
+def uncorr_nstep(agent, sigma):
+    """
+    Apply uncorrelated update on genes of agent
+    Args:
+        agent (nn): An agent
+        sigma (np.ndarray): step size for each gene
+    """
