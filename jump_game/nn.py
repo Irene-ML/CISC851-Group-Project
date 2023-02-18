aeceeb21ad2aa2ac6224e22ba2cd9cf86fea2fae
Chen Xu <chenxu@chens-mbp.lan>
2023-02-17 19:29:40 -0500

    Neural Network model that works as Agent

--- jump_game/nn.py
+++ jump_game/nn.py
@@ -1,3 +1,56 @@
 """_description_
 """
+import numpy as np
 
+class Agent:
+    """
+        Create Agent (player) object
+    """
+    
+    def __init__(self, n_feature, n_hidden, n_out):
+        """
+        Initialization the Agent
+        
+        Args:
+            n_feature (int): number of features that fed into neural network
+            n_hidden (int): number of nodes at hidden layer
+            n_out (int): number of nodes at output layer
+        """
+        self.n_feature = n_feature
+        self.n_hidden = n_hidden
+        self.n_out = n_out
+        self.w_ih = np.random.rand(n_feature, n_hidden)
+        self.w_ho = np.random.rand(n_hidden, n_out)
+        
+    def prediction(self, input_features, activation = "sigmoid"):
+        """
+        Define prediction function that predicts the x, y components of acceleration
+        Args:
+            input_features (np.ndarray): feature values of the ball object
+            activation (String): activation function that is used at hidden layer
+        """
+        if activation == "sigmoid":
+            return self.sigmoid(np.dot(self.sigmoid(np.dot(input_features, self.w_ih)), self.w_ho))
+        
+        elif activation == "relu":
+            return self.sigmoid(np.dot(self.relu(np.dot(input_features, self.w_ih)), self.w_ho))
+            
+        else:
+            raise Exception("use 'sigmoid' or 'relu' for second arg")
+    
+    
+    def sigmoid(self, x):
+        """
+        Define sigmoid function
+        Args:
+            x (numpy.ndarray): output scores at certain layer
+        """
+        return np.array(1. / (1. + np.exp(-x)))
+    
+    def relu(self, x):
+        """
+        Define relu funciton
+        Args:
+            x (numpy.ndarray): output scores at certain layer
+        """
+        return np.maximum(0, x);
