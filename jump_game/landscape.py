fa4333de819c20bb1fad833cb07dd150cf191d78
Chen Xu <chenxu@chens-mbp.lan>
2023-02-28 00:47:31 -0500

    Creat landscape with n obtacles

--- jump_game/landscape.py
+++ jump_game/landscape.py
@@ -1,4 +1,4 @@
-"""Create landscapes
+"""Create landscape
 """
 from obstacles import Obstacle
 from obstacles import Obstacle
@@ -6,9 +6,9 @@ from constants import GRAVITY, TIME_INTERVAL
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from constants import BALL_COLOR, BALL_RADIUS, BALL_VELOCITY_X, BALL_VELOCITY_Y, BALL_X
 from constants import OBSTACLE_GAP, OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_VELOCITY
-def create_landscapes(n):
+def create_landscape(n):
     """
     Args:
-        n (int): how many landscapes to be generated
+        n (int): how many obstacles to be generated
     """
     return [Obstacle(BALL_X + OBSTACLE_GAP + OBSTACLE_GAP, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 0) for _ in range(n)]
