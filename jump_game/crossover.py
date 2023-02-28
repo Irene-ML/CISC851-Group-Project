981a389025788a22171cfff26ca6f3ef41e5aef7
Chen Xu <chenxu@chens-mbp.lan>
2023-02-28 10:45:49 -0500

    Added random sampled crossover

--- jump_game/crossover.py
+++ jump_game/crossover.py
@@ -1,13 +1,14 @@
 """_description_
 """
 import nn
+import numpy as np
 
-def crossover(agent1, agent2):
-        """
-        Apply crossover on the chronosome of two agents
-        Args:
-            agent1, agent2 (nn): nn objects
-        """
+def balk_crossover(agent1, agent2):
+    """
+    Apply balk crossover on the chronosome of two agents
+    Args:
+        agent1, agent2 (nn): nn objects
+    """
     offspring1 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
     offspring2 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
     
@@ -18,3 +19,24 @@ def crossover(agent1, agent2):
     offspring2.w_ho = agent1.w_ho
 
     return offspring1, offspring2
+    
+def sample_crossover(agent1, agent2, rate):
+    """
+    Apply sampled crossover on the chronosome of two agents
+    Args:
+        agent1, agent2 (nn): nn objects
+        rate (float): probability that one gene would be switched
+    """
+    mask_ih = np.random.rand(agent1.n_feature, agent1.n_hidden) <= rate
+    mask_ho = np.random.rand(agent1.n_hidden, agent1.n_out) <= rate
+    
+    offspring1 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring2 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+
+    offspring1.w_ih = np.multiply(agent1.w_ih, 1 - mask_ih) + np.multiply(agent2.w_ih, mask_ih)
+    offspring1.w_ho = np.multiply(agent1.w_ho, 1 - mask_ho) + np.multiply(agent2.w_ho, mask_ho)
+    
+    offspring2.w_ih = np.multiply(agent1.w_ih, mask_ih) + np.multiply(agent2.w_ih, 1 - mask_ih)
+    offspring2.w_ho = np.multiply(agent1.w_ho, mask_ho) + np.multiply(agent2.w_ho, 1 - mask_ho)
+    
+    return offspring1. offspring2
