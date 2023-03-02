8aff37cbf6c45fab5d7c8c49427114605aecbfb9
Chen Xu <chenxu@chens-mbp.lan>
2023-03-01 21:43:26 -0500

    Fixed bug

--- jump_game/crossover.py
+++ jump_game/crossover.py
@@ -9,8 +9,8 @@ def balk_crossover(agent1, agent2):
     Args:
         agent1, agent2 (nn): nn objects
     """
-    offspring1 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
-    offspring2 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring1 = Agent(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring2 = Agent(agent1.n_feature, agent1.n_hidden, agent1.n_out)
     
     offspring1.w_ih = agent1.w_ih
     offspring1.w_ho = agent2.w_ho
@@ -30,8 +30,8 @@ def sample_crossover(agent1, agent2, rate):
     mask_ih = np.random.rand(agent1.n_feature, agent1.n_hidden) <= rate
     mask_ho = np.random.rand(agent1.n_hidden, agent1.n_out) <= rate
     
-    offspring1 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
-    offspring2 = nn(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring1 = Agent(agent1.n_feature, agent1.n_hidden, agent1.n_out)
+    offspring2 = Agent(agent1.n_feature, agent1.n_hidden, agent1.n_out)
 
     offspring1.w_ih = np.multiply(agent1.w_ih, 1 - mask_ih) + np.multiply(agent2.w_ih, mask_ih)
     offspring1.w_ho = np.multiply(agent1.w_ho, 1 - mask_ho) + np.multiply(agent2.w_ho, mask_ho)
@@ -39,4 +39,4 @@ def sample_crossover(agent1, agent2, rate):
     offspring2.w_ih = np.multiply(agent1.w_ih, mask_ih) + np.multiply(agent2.w_ih, 1 - mask_ih)
     offspring2.w_ho = np.multiply(agent1.w_ho, mask_ho) + np.multiply(agent2.w_ho, 1 - mask_ho)
     
-    return offspring1. offspring2
+    return offspring1, offspring2
