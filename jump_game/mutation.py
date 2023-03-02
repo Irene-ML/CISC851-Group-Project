9cf654ad3ca8807017bfc184783beb603daabc85
Chen Xu <chenxu@chens-mbp.lan>
2023-03-01 21:44:35 -0500

    Fixed bug

--- jump_game/mutation.py
+++ jump_game/mutation.py
@@ -1,6 +1,8 @@
 """_description_
 """
-import nn
+from nn import Agent
+import numpy as np
+
 
 def uncorr_onestep(agent, sigma):
     """
@@ -9,8 +11,8 @@ def uncorr_onestep(agent, sigma):
         agent (nn): An agent
         sigma (float): step size
     """
-    delta_ih = np.random.normal(0, sigma, (nn.n_feature, nn.n_hidden))
-    delta_ho = np.random.normal(0, sigma, (nn.n_hidden, nn.n_out))
+    delta_ih = np.random.normal(0, sigma, (agent.n_feature, agent.n_hidden))
+    delta_ho = np.random.normal(0, sigma, (agent.n_hidden, agent.n_out))
     agent.w_ih = agent.w_ih + delta_ih
     agent.w_ho = agent.w_ho + delta_ho
     
@@ -22,8 +24,8 @@ def uncorr_nstep(agent, sigma_ih, sigma_ho):
         sigma_ih (np.ndarray): step size for each gene at input layer
         sigma_ho (np.ndarray): step size for each gene at output layer
     """
-    delta_ih = np.multiply(p.random.normal(0, 1, (nn.n_feature, nn.n_hidden)), sigma_ih)
-    delta_ho = np.multiply(p.random.normal(0, 1, (nn.n_hidden, nn.n_out)), sigma_ho)
+    delta_ih = np.multiply(p.random.normal(0, 1, (agent.n_feature, agent.n_hidden)), sigma_ih)
+    delta_ho = np.multiply(p.random.normal(0, 1, (agent.n_hidden, agent.n_out)), sigma_ho)
     agent.w_ih = agent.w_ih + delta_ih
     agent.w_ho = agent.w_ho + delta_ho
     
