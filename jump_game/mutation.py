1a0d9b470c31fe758354c9f6e64d5716c67bc6e8
Chen Xu <chenxu@chens-mbp.lan>
2023-03-22 11:11:31 -0400

    Modified uncorrelated nstep function

--- jump_game/mutation.py
+++ jump_game/mutation.py
@@ -19,20 +19,18 @@ def uncorr_onestep(agent, sigma, rate):
     agent.w_ih = agent.w_ih + np.multiply(delta_ih, mask_ih)
     agent.w_ho = agent.w_ho + np.multiply(delta_ho, mask_ho)
     
-def uncorr_nstep(agent, sigma_ih, sigma_ho, rate):
+def uncorr_nstep(agent, sigma, rate):
     """
     Apply uncorrelated update on genes of agent
     Args:
         agent (nn): An agent
-        sigmas (list(np.ndarray): contains 
-        sigma_ih (np.ndarray): step size for each gene at input layer
-        sigma_ho (np.ndarray): step size for each gene at output layer
+        sigma (float): a scale that applied on n-step vector
         rate (float): mutation rate
     """
-    delta_ih = np.multiply(np.random.normal(0, 1, (agent.n_feature, agent.n_hidden)), sigma_ih)
-    delta_ho = np.multiply(np.random.normal(0, 1, (agent.n_hidden, agent.n_out)), sigma_ho)
-    mask_ih = np.random,rand(agent.n_feature, agent.n_hidden) < rate
-    mask_ho = np.random,rand(agent.n_hidden, agent.n_out) < rate
+    delta_ih = np.multiply(np.random.normal(0, 1, (agent.n_feature, agent.n_hidden)), agent. sigma_ih) * sigma
+    delta_ho = np.multiply(np.random.normal(0, 1, (agent.n_hidden, agent.n_out)), agent.sigma_ho) * sigma
+    mask_ih = np.random.rand(agent.n_feature, agent.n_hidden) < rate
+    mask_ho = np.random.rand(agent.n_hidden, agent.n_out) < rate
     agent.w_ih = agent.w_ih + np.multiply(delta_ih, mask_ih)
     agent.w_ho = agent.w_ho + np.multiply(delta_ho, mask_ho)
 
