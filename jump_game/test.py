5055bf23762186aace0e2a79b6fb619d9e827090
Chen Xu <chenxu@Chens-MacBook-Pro.local>
2023-03-23 14:54:14 -0400

    Modified it for parallel running

--- jump_game/test.py
+++ jump_game/test.py
@@ -1,8 +1,9 @@
 import itertools
 import json
 import os
+import sys
+import shutil
 import numpy as np
-import ea
 from constants import HYPER_PARAMS, NUMBER_OF_RUNNING
 from log import logging
 
@@ -32,26 +33,39 @@ def file_generator(combinations, params, path, RUNNING_ENABLED=True):
         if not os.path.exists(new_path):
             os.makedirs(new_path)
             print("Directory '% s' created" % new_path)
+            src = os.listdir("./")
+            for file in src:
+                if file[-2:] != "py" or file == "test.py":
+                    continue
+                des = f"{path}/{i+1}/"
+                shutil.copy(file, des)
+            print("files copied into % s' created" % new_path)
 
         input_filename = f"{new_path}/input_params.json"
         with open(input_filename, 'w') as f:
             json.dump(sample_params, f, indent=4)
         logging.info(f"Wrote to the file: {input_filename}")
 
-        if RUNNING_ENABLED:
-            result = ea.main(sample_params)
-            output_filename = f"{new_path}/output.json"
-            with open(output_filename, 'w') as f:
-                json.dump(result, f, indent=4)
-            logging.info(f"Wrote to the file: {output_filename}")
+#        if RUNNING_ENABLED:
+#            result = ea.main(sample_params)
+#            output_filename = f"{new_path}/output.json"
+#            with open(output_filename, 'w') as f:
+#                json.dump(result, f, indent=4)
+#            logging.info(f"Wrote to the file: {output_filename}")
         
 
 if __name__ == "__main__":
-    test_path="./test"
-    if not os.path.exists(test_path):
-        os.makedirs(test_path)
-        print("Directory '% s' created" % test_path)
+    if len(sys.argv) == 0:
+        print("Please assign test number for this test and run something like: python3 test.py 2.")
+    else:
+        test_number = sys.argv[1]
+        test_path = f"./test{test_number}"
+        if os.path.exists(test_path):
+            print(f"Assign different number for this test rather than {test_number}" )
+        else:
+            os.makedirs(test_path)
+            print("Directory '% s' created" % test_path)
     
-    for n in range(NUMBER_OF_RUNNING):
-        if len(param_combinations) == num_combinations:
-            file_generator(param_combinations, HYPER_PARAMS, f"{test_path}/test_{n+1}")
+            for n in range(NUMBER_OF_RUNNING):
+                if len(param_combinations) == num_combinations:
+                    file_generator(param_combinations, HYPER_PARAMS, f"{test_path}/test_{n+1}")
